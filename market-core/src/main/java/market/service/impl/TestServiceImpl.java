cc255e213f8c10cc06828df89eba15c785ef44fd
María José Suárez-Cabal <cabal@uniovi.es>
2024-05-02 18:14:51 +0200

    Actualizacion servicio de test

--- market-core/src/main/java/market/service/impl/TestServiceImpl.java
+++ market-core/src/main/java/market/service/impl/TestServiceImpl.java
@@ -13,6 +13,7 @@ import market.dao.CartDAO;
 import market.dao.ContactsDAO;
 import market.dao.DistilleryDAO;
 import market.dao.OrderDAO;
+import market.dao.OrderedProductDAO;
 import market.dao.ProductDAO;
 import market.dao.RegionDAO;
 import market.dao.UserAccountDAO;
@@ -20,6 +21,7 @@ import market.domain.Cart;
 import market.domain.Contacts;
 import market.domain.Distillery;
 import market.domain.Order;
+import market.domain.OrderedProduct;
 import market.domain.Product;
 import market.domain.Region;
 import market.domain.UserAccount;
@@ -42,6 +44,7 @@ public class TestServiceImpl implements TestService {
     	public List<Contacts> contacts=new ArrayList<>();
     	public List<Distillery> distillery=new ArrayList<>();
     	public List<Order> order=new ArrayList<>();
+    	public List<OrderedProduct> orderedProduct=new ArrayList<>();
     	public List<Product> product=new ArrayList<>();
     	public List<Region> region=new ArrayList<>();
     	public List<UserAccount> userAccount=new ArrayList<>();
@@ -51,16 +54,18 @@ public class TestServiceImpl implements TestService {
 	private final ContactsDAO contactsDAO;
 	private final DistilleryDAO distilleryDAO;
 	private final OrderDAO orderDAO;
+	private final OrderedProductDAO orderedProductDAO;
 	private final ProductDAO productDAO;
 	private final RegionDAO regionDAO;
 	private final UserAccountDAO userAccountDAO;
 	
-	public TestServiceImpl(CartDAO cartDAO, ContactsDAO contactsDAO, DistilleryDAO distilleryDAO, OrderDAO orderDAO, ProductDAO productDAO, 
-			RegionDAO regionDAO, UserAccountDAO userAccountDAO) {
+	public TestServiceImpl(CartDAO cartDAO, ContactsDAO contactsDAO, DistilleryDAO distilleryDAO, OrderDAO orderDAO, OrderedProductDAO orderedProductDAO, 
+			ProductDAO productDAO,RegionDAO regionDAO, UserAccountDAO userAccountDAO) {
 		this.cartDAO= cartDAO;
 		this.contactsDAO= contactsDAO;
 		this.distilleryDAO= distilleryDAO;
 		this.orderDAO=orderDAO;
+		this.orderedProductDAO=orderedProductDAO;
 		this.productDAO=productDAO;
 		this.regionDAO= regionDAO;
 		this.userAccountDAO= userAccountDAO;
@@ -68,6 +73,7 @@ public class TestServiceImpl implements TestService {
 	
 	@Override
 	public void deleteAll() throws SQLException {
+		orderedProductDAO.deleteAll();
 		orderDAO.deleteAll();
 		cartDAO.deleteAll();
 		contactsDAO.deleteAll();
@@ -76,8 +82,10 @@ public class TestServiceImpl implements TestService {
 		distilleryDAO.deleteAll();
 		regionDAO.deleteAll();
 		DbTestUtil.resetAutoIncrementColumns(applicationContext, 
+				"bill",
 				"ordered_product",
-				"customer_order", 
+				"customer_order",
+				"cart_item",
 				"cart", 
 				"contacts", 
 				"user_account", 
@@ -97,6 +105,7 @@ public class TestServiceImpl implements TestService {
 		data.contacts=contactsDAO.findAll();
 		data.cart= cartDAO.findAll();
 		data.order=orderDAO.findAll();
+		data.orderedProduct=orderedProductDAO.findAll();
 
 		data.userAccount.forEach(e -> e.setPassword("hidden"));
 		return data;
